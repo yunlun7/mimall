1f1aa0d2a46b5161480e72d6fbbd7ad2584adcb9
yunlu7 <2733734845@qq.com>
2022-10-26 23:46:05 +0800

    路由按需加载

--- src/router.js
+++ src/router.js
@@ -3,15 +3,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './pages/home'
 import Index from './pages/index'
-import Login from './pages/login'
-import Product from './pages/product'
-import Detail from './pages/detail'
-import Cart from './pages/cart'
-import Order from './pages/order'
-import OrderConfirm from './pages/orderConfirm'
-import OrderList from './pages/orderList'
-import OrderPay from './pages/orderPay'
-import AliPay from './pages/alipay'
+// import Login from './pages/login'
+// import Product from './pages/product'
+// import Detail from './pages/detail'
+// import Cart from './pages/cart'
+// import Order from './pages/order'
+// import OrderConfirm from './pages/orderConfirm'
+// import OrderList from './pages/orderList'
+// import OrderPay from './pages/orderPay'
+// import AliPay from './pages/alipay'
 
 // 加载插件固定的语法，这样代表已经将vue插件加载进去了
 Vue.use(Router)
@@ -42,13 +42,14 @@ export default new Router({
           // 引入产品站，此处是一个动态的子路由
           path: '/product/:id',
           name: 'product',
-          component: Product
+          component: () => import('./pages/product.vue') // 路由按需加载方案二
+          // component: resolve => require(['./pages/product.vue'], resolve) // 路由按需加载方案一
         },
         {
           // 引入详情页面,此处是一个动态的子路由
           path: '/detail/:id',
           name: 'detail',
-          component: Detail
+          component: () => import('./pages/detail.vue')
         }
       ]
     },
@@ -56,43 +57,43 @@ export default new Router({
       // 定义登录路由
       path: '/login',
       name: 'login',
-      component: Login
+      component: () => import('./pages/login.vue')
     },
     // 因为部分的页面视图和home渲染的不一样，需要再定义一个路由
     {
       // 定义购物车路由
       path: '/cart',
       name: 'cart',
-      component: Cart
+      component: () => import('./pages/cart.vue')
     },
     {
       // 定义订单页面
       path: '/order',
       name: 'order',
-      component: Order,
+      component: () => import('./pages/order.vue'),
       children: [
         {
           // 订单详情
           path: 'confirm',
           name: 'OrderConfirm',
-          component: OrderConfirm
+          component: () => import('./pages/orderConfirm.vue')
         },
         {
           // 订单列表
           path: 'list',
           name: 'OrderList',
-          component: OrderList
+          component: () => import('./pages/orderList.vue') // 路由按需加载方案二
         },
         {
           // 订单支付
           path: 'pay',
           name: 'OrderPay',
-          component: OrderPay
+          component: () => import('./pages/orderPay.vue')
         },
         {
           path: 'alipay',
           name: 'AliPay',
-          component: AliPay
+          component: () => import('./pages/alipay.vue')
         }
       ]
     }
